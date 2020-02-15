e709c1abe1125bdae4ae80245639ec5087f286e3
sidearrow <sidearrow@gmail.com>
2020-02-15 08:40:20 +0000

    add login

--- webapp/pyramid/app/__init__.py
+++ webapp/pyramid/app/__init__.py
@@ -1,8 +1,11 @@
 from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
 
+session_factory = SignedCookieSessionFactory('pyramid')
 
 def main(global_config, **settings):
     with Configurator(settings=settings) as config:
+        config.set_session_factory(session_factory)
         config.add_renderer('.html', 'pyramid_jinja2.renderer_factory')
         config.include('pyramid_jinja2')
         config.include('.app')
--- webapp/pyramid/app/app.py
+++ webapp/pyramid/app/app.py
@@ -1,17 +1,55 @@
+import hashlib
+import pymysql
+from pyramid import httpexceptions
 from pyramid.view import view_config
 from pyramid.request import Request
 from pyramid.response import Response
 from pyramid.config import Configurator
 
 
+db_connection = pymysql.connections.Connection(
+    host='localhost',
+    user='isucon',
+    password='isucon',
+    db='isubata',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
 @view_config(
-    route_name='index_get',
+    route_name='index',
     renderer='./templates/index.html',
 )
-def index_get(request: Request):
+def action_index_get(request: Request):
     return {}
+
+
+@view_config(
+    route_name='login',
+    renderer='./templates/login.html',
+    request_method='GET',
+)
+def action_login_get(request: Request):
+    return {}
+
+
+@view_config(
+    route_name='login',
+    request_method='POST',
+)
+def action_login_post(request: Request):
+    name = request.POST['name']
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM user WHERE name = %s", (name,))
+    row = cursor.fetchone()
+    if not row or row['password'] != hashlib.sha1((row['salt'] + request.POST['password']).encode('utf-8')).hexdigest():
+        raise httpexceptions.exception_response(403)
+    request.session['user_id'] = row['id']
+    return httpexceptions.HTTPPermanentRedirect('/')
 
 
 def includeme(config: Configurator):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('index_get', '/')
+    config.add_route('index', '/')
+    config.add_route('login', '/login')
