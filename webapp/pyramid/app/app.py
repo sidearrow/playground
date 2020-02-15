03767a3abf00f98a4994fc1f65c891e05334d8c4
sidearrow <sidearrow@gmail.com>
2020-02-15 09:38:12 +0000

    add message

--- webapp/pyramid/app/app.py
+++ webapp/pyramid/app/app.py
@@ -80,8 +80,46 @@ def action_channel(request: Request):
     }
 
 
+@view_config(
+    route_name='message',
+    renderer='json'
+)
+def action_message(request: Request):
+    if 'user_id' not in request.session:
+        httpexceptions.exception_response(403)
+
+    channel_id = request.GET['channel_id']
+    last_message_id = request.GET['last_message_id']
+    cursor = db_connection.cursor()
+    cursor.execute(
+        "select msg.id, msg.created_at, msg.content, usr.name, usr.display_name, usr.avatar_icon"
+        " from message msg"
+        " left join user usr on usr.id = msg.user_id"
+        " where msg.id > %s and msg.channel_id = %s"
+        " order by msg.id desc limit 100",
+        (last_message_id, channel_id)
+    )
+    rows = cursor.fetchall()
+    response = []
+    for row in rows:
+        r = {}
+        r['id'] = row['id']
+        r['user'] = {
+            'name': row['name'],
+            'display_name': row['display_name'],
+            'avatar_icon': row['avatar_icon'],
+        }
+        r['date'] = row['created_at'].strftime("%Y/%m/%d %H:%M:%S")
+        r['content'] = row['content']
+        response.append(r)
+    response.reverse()
+
+    return response
+
+
 def includeme(config: Configurator):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('index', '/')
     config.add_route('login', '/login')
-    config.add_route('channel', 'channel/{channel_id}')
+    config.add_route('channel', '/channel/{channel_id}')
+    config.add_route('message', '/message')
