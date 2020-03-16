af086f1e897d18c47f7286447d1f4abf8be61420
sidearrow <sidearrow1219@gmail.com>
2020-03-17 00:42:09 +0900

    [data] add get station list

--- data_pp/collect_station_list/station_list.py
+++ data_pp/collect_station_list/station_list.py
@@ -1,10 +1,29 @@
 import urllib
 import requests
+from bs4 import BeautifulSoup
 
-BASE_URL = 'https://ja.wikipedia.org/wiki/'
+BASE_URL = 'https://ja.wikipedia.org'
 
-url = BASE_URL + urllib.parse.quote('日本の鉄道駅一覧')
 
-res = requests.get(url)
+def get_list_html():
+    res = requests.get(BASE_URL + urllib.parse.quote('/wiki/日本の鉄道駅一覧'))
+    open('./list.html', mode='w').write(res.text)
 
-print(res.text)
+
+def get_list():
+    html = open('./list.html').read()
+    bs = BeautifulSoup(html, 'html.parser')
+    link_list = bs.select('td > a')
+
+    res = []
+    for link in link_list:
+        title = link.get('title')
+        href = link.get('href')
+        if '日本の鉄道駅一覧' in title:
+            res.append(href)
+
+    return res
+
+
+link_list = get_list()
+print(link_list)
