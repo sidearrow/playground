0d4b54fe29de5f6dd6f7e22e7e46c828ab546e9d
sidearrow <sidearrow1219@gmail.com>
2020-04-08 09:18:03 +0900

    mod

--- routes/route.php
+++ routes/route.php
@@ -6,3 +6,5 @@ Route::get('company', 'CompanyIndexController');
 Route::get('company/{companyCode}/line', 'CompanyLineIndexController');
 Route::get('line', 'LineController');
 Route::get('line/{lineCode}', 'LineDetailController');
+
+Route::post('station/bulk-update', 'StationBulkUpdateController');
