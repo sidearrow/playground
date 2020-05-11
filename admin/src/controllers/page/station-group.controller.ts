83725e54a1a938a030ef2863931e9af7ddea4b90
sidearrow <sidearrow1219@gmail.com>
2020-05-11 21:46:54 +0900

    [admin] mod

--- admin/src/controllers/page/station-group.controller.ts
+++ admin/src/controllers/page/station-group.controller.ts
@@ -1,7 +1,34 @@
-import { Controller, Post, BodyParam } from 'routing-controllers';
+import {
+  Controller,
+  Post,
+  BodyParam,
+  Res,
+  Req,
+  Param,
+} from 'routing-controllers';
+import { Response, Request } from 'express';
 
 @Controller('/station-group')
 export class StationGroupController {
   @Post('/')
-  async create(@BodyParam('stationId')) { }
+  async create(
+    @Req() req: Request,
+    @Res() res: Response,
+    @BodyParam('stationId') stationId: string
+  ): Promise<Response> {
+    res.redirect(req.headers.referer);
+
+    return res;
+  }
+
+  @Post('/:stationGroupId')
+  async add(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Param('stationGroupId') stationGroupId: string
+  ): Promise<Response> {
+    res.redirect(req.headers.referer);
+
+    return res;
+  }
 }
