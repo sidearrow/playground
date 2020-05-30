5e3a3908b6aa05b880a2b73e8d40892a06d5d189
sidearrow <sidearrow1219@gmail.com>
2020-05-30 11:15:30 +0900

    [*] remove unused directory

--- admin-page/resources/views/pages/company/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-
-@section('main')
-<section>
-  @foreach ($companies as $company)
-  <span class="d-inline-block">{{ $company['companyName'] }}</span>
-  @endforeach
-</section>
-@endsection
