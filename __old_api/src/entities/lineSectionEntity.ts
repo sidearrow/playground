5f8b62ec62a0c5c963902928287af01a2ddef8f7
sidearrow <sidearrow1219@gmail.com>
2020-06-03 18:20:37 +0900

    [*] mod directory structure

--- __old_api/src/entities/lineSectionEntity.ts
+++
@@ -1,9 +0,0 @@
-import { StationEntity } from './stationEntity';
-
-export type LineSectionEntity = {
-  readonly lineId: number;
-  readonly sectionId: number;
-  readonly lineSectionName: string;
-
-  readonly stations: StationEntity[];
-};
