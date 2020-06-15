54adf03f42a101f0d9288e4e711bc5ff8a6bab6c
sidearrow <sidearrow1219@gmail.com>
2020-06-15 19:46:45 +0900

    [api] mod

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,26 +1,29 @@
 <?php
 
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', 'AuthController@login');
 Route::get('/auth-check', 'AuthController@check');
 
-Route::prefix('/company')->group(function () {
-    Route::get('/', 'CompanyController@index');
+Route::middleware([AuthMiddleware::class])->group(function () {
+    Route::prefix('/company')->group(function () {
+        Route::get('/', 'CompanyController@index');
 
-    Route::get('/code={companyCode}', 'CompanyController@getOneByCode');
-    Route::get('/{companyId}', 'CompanyController@getOne');
+        Route::get('/code={companyCode}', 'CompanyController@getOneByCode');
+        Route::get('/{companyId}', 'CompanyController@getOne');
 
-    Route::get('/{companyId}/line', 'CompanyController@getLines');
-    Route::get('/{companyId}/statistics', 'CompanyController@getStatistics');
-});
+        Route::get('/{companyId}/line', 'CompanyController@getLines');
+        Route::get('/{companyId}/statistics', 'CompanyController@getStatistics');
+    });
 
-Route::prefix('/line')->group(function () {
-    Route::get('/', 'LineController@getAll');
-    Route::get('/{lineId}', 'LineController@getDetail');
-});
+    Route::prefix('/line')->group(function () {
+        Route::get('/', 'LineController@getAll');
+        Route::get('/{lineId}', 'LineController@getDetail');
+    });
 
-Route::prefix('/station')->group(function () {
-    Route::get('/', 'StationController@get');
-    Route::put('/{stationId}/group-station', 'StationController@groupStationUpdate');
+    Route::prefix('/station')->group(function () {
+        Route::get('/', 'StationController@get');
+        Route::put('/{stationId}/group-station', 'StationController@groupStationUpdate');
+    });
 });
