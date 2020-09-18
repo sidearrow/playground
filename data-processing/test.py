2f1dd8a10f3b640dcda391b8a5f793d780e54201
sidearrow <sidearrow1219@gmail.com>
2020-09-18 22:43:59 +0900

    Fixed

--- data-processing/test.py
+++ data-processing/test.py
@@ -3,6 +3,7 @@ import typing
 import psycopg2
 from shapely import wkb, ops
 from shapely.geometry import LineString, Point
+from shapely.geometry.multilinestring import MultiLineString
 
 
 con = psycopg2.connect(host='localhost', port='15432', user='user', password='password', dbname='express_map')
@@ -18,8 +19,10 @@ def split(line_string: LineString, point: Point) -> typing.Tuple[LineString, Lin
             break
     if j is None:
         raise Exception("Point doesn't exists in line.")
+    if j == 0:
+        return None, line_string
     if j + 2 == len(coords):
-        return LineString(coords[:j + 2]), None
+        return line_string, None
     if Point(coords[j + 1:j + 2]).equals(point):
         return LineString(coords[:j + 2]), LineString(coords[j + 1:])
     return LineString(coords[:j + 1]), LineString(coords[j:])
@@ -37,20 +40,21 @@ def get_line(id: int) -> str:
     return res[0]
 
 
-with open('./haruka.json') as f:
+with open('./uzushio.json') as f:
     config = json.load(f)
 
 line_config = config['line']
 
+line_sections = []
 for section_config in config['line']:
     start_station = wkb.loads(get_station(section_config['start']['id']), hex=True)
     end_station = wkb.loads(get_station(section_config['end']['id']), hex=True)
     line = wkb.loads(get_line(section_config['line']['id']), hex=True)
 
     if line.intersects(start_station) is False:
-        raise Exception("Start station doesn't exists in line.")
+        raise Exception("Start station doesn't exists in line.: {}".format(section_config['start']['remarks']))
     if line.intersects(end_station) is False:
-        raise Exception("End station doesn't exists in line.")
+        raise Exception("End station doesn't exists in line.: {}".format(section_config['end']['remarks']))
 
     split_start_lines = split(line, start_station)
     tmp_line = split_start_lines[0] \
@@ -62,4 +66,8 @@ for section_config in config['line']:
         if split_lines[0] is not None and split_lines[0].intersects(start_station) \
         else split_lines[1]
 
-    print(split_line)
+    line_sections.append(split_line)
+
+line = MultiLineString(line_sections)
+
+print(line)
