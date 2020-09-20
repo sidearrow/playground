acfacc0a4fe1b7b99779fc83c7f7337c748c7bbf
sidearrow <sidearrow1219@gmail.com>
2020-09-20 22:51:15 +0900

    FIxed

--- front/src/libs/trainMap.ts
+++ front/src/libs/trainMap.ts
@@ -1,16 +1,12 @@
 import { Map, View } from 'ol';
 import { VectorTile as VectorTileLayer, Tile as TileLayer } from 'ol/layer';
 import { VectorTile as VectorTileSource, XYZ as XYZSource } from 'ol/source';
-import { MVT as MVTFormat, GeoJSON as GeoJSONFormat } from 'ol/format';
+import { MVT as MVTFormat } from 'ol/format';
 import { fromLonLat } from 'ol/proj';
 
 import { CONFIG } from '../config';
 import { Style, Stroke, Circle, Fill } from 'ol/style';
-import { GeoJSONFeatureCollection } from 'ol/format/GeoJSON';
-
-import TRAIN_STATIONS_JSON from '../train_stations.json';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
+import { StyleFunction } from 'ol/style/Style';
 
 type TrainProps = {
   code: string;
@@ -19,48 +15,74 @@ type TrainProps = {
   stations: string;
 };
 
-const LAYER_NAMES = {
-  LINE: 'line',
-  TRAIN: 'train',
-};
-
-const LINE_STYLE = new Style({
-  stroke: new Stroke({
-    color: '#a0aec0',
-    width: 1,
-  }),
-  zIndex: 1,
-});
-
-const TRAIN_STYLE = (color: string) =>
-  new Style({
-    stroke: new Stroke({
-      color: color,
-      width: 5,
-    }),
-    zIndex: 2,
-  });
-
-const TRAIN_STATION_STYLE = new Style({
-  image: new Circle({
-    radius: 10,
-    stroke: new Stroke({
-      color: '#ac2926',
-      width: 10,
-    }),
-    fill: new Fill({
-      color: '#ac2926',
-    }),
-  }),
-  zIndex: 3,
-});
-
-function getTrainStationsGeoJSON(code: 'uzushio'): GeoJSONFeatureCollection {
-  return TRAIN_STATIONS_JSON[code] as GeoJSONFeatureCollection;
-}
-
 export class TrainMap {
   private map: Map;
+  private selectedTrainCode: string | null = null;
+
+  private LAYER_NAMES = {
+    LINE: 'line',
+    TRAIN: 'train',
+    TRAIN_STATION: 'train_station',
+  };
+
+  private trainStyle = (color: string, isSelected: boolean) =>
+    new Style({
+      stroke: new Stroke({
+        color: color,
+        width: isSelected ? 5 : 3,
+      }),
+      zIndex: 2,
+    });
+
+  private trainStationStyle = new Style({
+    image: new Circle({
+      radius: 10,
+      stroke: new Stroke({
+        color: '#ac2926',
+        width: 10,
+      }),
+      fill: new Fill({
+        color: '#ac2926',
+      }),
+    }),
+    zIndex: 3,
+  });
+
+  private lineStyle = new Style({
+    stroke: new Stroke({
+      color: '#a0aec0',
+      width: 1,
+    }),
+    zIndex: 1,
+  });
+
+  private mainLayerStyleFunc: StyleFunction = (feature) => {
+    const props = feature.getProperties();
+    const layer = props.layer;
+    console.log(layer);
+    if (layer === this.LAYER_NAMES.LINE) {
+      return this.lineStyle;
+    }
+    if (layer === this.LAYER_NAMES.TRAIN_STATION) {
+      return this.trainStationStyle;
+    }
+    const color = props.color;
+    return this.trainStyle(color, props.train_code === this.selectedTrainCode);
+  };
+
+  private baseLayer = new TileLayer({
+    source: new XYZSource({
+      url: CONFIG.BASE_MAP_URL,
+    }),
+  });
+
+  private mainLayer = new VectorTileLayer({
+    source: new VectorTileSource({
+      format: new MVTFormat(),
+      url: CONFIG.TRAIN_MAP_URL,
+    }),
+    style: this.mainLayerStyleFunc,
+  });
 
   public constructor() {
     this.map = new Map({
@@ -69,61 +91,21 @@ export class TrainMap {
         center: fromLonLat([135, 35]),
         zoom: 10,
       }),
-      layers: [
-        new TileLayer({
-          source: new XYZSource({
-            url: CONFIG.BASE_MAP_URL,
-          }),
-        }),
-        new VectorTileLayer({
-          source: new VectorTileSource({
-            format: new MVTFormat(),
-            url: CONFIG.TRAIN_MAP_URL,
-          }),
-          style: (feature) => {
-            const props = feature.getProperties();
-            const layer = props.layer;
-            if (layer === 'line') {
-              return LINE_STYLE;
-            }
-            const color = props.color;
-            return TRAIN_STYLE(color);
-          },
-        }),
-        new VectorLayer({
-          source: new VectorSource({
-            features: new GeoJSONFormat().readFeatures(
-              TRAIN_STATIONS_JSON.uzushio
-            ),
-          }),
-          style: TRAIN_STATION_STYLE,
-        }),
-      ],
+      layers: [this.baseLayer, this.mainLayer],
     });
     this.map.on('click', (e) => {
       const pixel = this.map.getEventPixel(e.originalEvent);
       const features = this.map.getFeaturesAtPixel(pixel);
-      console.log(features);
       const feature = features.find(
-        (f) => f.getProperties().layer === LAYER_NAMES.TRAINi
+        (f) => f.getProperties().layer === this.LAYER_NAMES.TRAIN
       );
       if (feature === undefined) {
-        return;
+        this.selectedTrainCode = null;
+      } else {
+        const props = feature.getProperties();
+        this.selectedTrainCode = props.train_code;
       }
-      console.log(feature);
-      const props = feature.getProperties();
-      const geojson = getTrainStationsGeoJSON(props.code);
-      console.log(geojson);
-      this.map.addLayer(
-        new VectorLayer({
-          source: new VectorSource({
-            format: new GeoJSONFormat(),
-            features: new GeoJSONFormat().readFeatures(geojson),
-          }),
-          style: TRAIN_STATION_STYLE,
-        })
-      );
-      console.log(this.map.getLayers());
+      this.mainLayer.setStyle(this.mainLayerStyleFunc);
     });
   }
 }
